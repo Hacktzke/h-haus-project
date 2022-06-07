6649f9ef710efefd70b24512e05e16e8bbd8a9aa
Jeshua <88014544+Hacktzke@users.noreply.github.com>
2022-06-07 12:22:00 +1200

    update, added footer

--- app.js
+++ app.js
@@ -5,33 +5,36 @@ const CART = document.querySelector(".cart");
 const CART_EXIT_ICON = document.querySelector("#exit-icon");
 
 const toggleSidenav = () => {
-    MENU_ICON.classList.toggle("change");
-    SIDENAV.classList.toggle("show-sidenav")
-    if (CART.classList.contains("show-cart")) {
-        closeCart();
-    }
-}
+  MENU_ICON.classList.toggle("change");
+  SIDENAV.classList.toggle("show-sidenav");
+  if (CART.classList.contains("show-cart")) {
+    closeCart();
+  }
+};
 
 const openCart = () => {
-    CART_ICON.classList.toggle("exit-change");
-    CART.classList.add("show-cart")
-    CART_EXIT_ICON.classList.add("open")
-    SIDENAV.classList.remove("show-sidenav")
-}
+  CART_ICON.classList.toggle("exit-change");
+  CART.classList.add("show-cart");
+  CART_EXIT_ICON.classList.add("open");
+  if (SIDENAV.classList.contains("show-sidenav")) {
+    MENU_ICON.classList.toggle("change");
+  }
+  SIDENAV.classList.remove("show-sidenav");
+};
 
 const closeCart = () => {
-    CART_EXIT_ICON.classList.remove("open");
-    CART.classList.remove("show-cart");
-}
+  CART_EXIT_ICON.classList.remove("open");
+  CART.classList.remove("show-cart");
+};
 
-MENU_ICON.addEventListener("click", function() {
-    toggleSidenav();
-})
+MENU_ICON.addEventListener("click", function () {
+  toggleSidenav();
+});
 
-CART_ICON.addEventListener("click", function() {
-    openCart()
-})
+CART_ICON.addEventListener("click", function () {
+  openCart();
+});
 
-CART_EXIT_ICON.addEventListener("click", function() {
-    closeCart()
-})
+CART_EXIT_ICON.addEventListener("click", function () {
+  closeCart();
+});
